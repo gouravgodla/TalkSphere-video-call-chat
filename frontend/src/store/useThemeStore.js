ec4f4972df9d5f1a08a9ab5d9ad12150747f9be7
gouravgodla <98866998+gouravgodla@users.noreply.github.com>
2025-10-08 00:32:08 +0530

    project-name-change

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("streamify-theme") || "coffee",
+    theme: localStorage.getItem("TalkSphere-theme") || "coffee",
     setTheme: (theme) => {
-        localStorage.setItem("streamify-theme", theme);
+        localStorage.setItem("TalkSphere-theme", theme);
         set({ theme });
     },
 }));
